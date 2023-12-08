f6601236b3ecc435b24ec811476f03152ceb03d7
johnzlli <1016963088@qq.com>
2023-12-08 19:03:16 +0800

    add cupy

---
+++ cupy_test/run.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+testdir=$(cd "$(dirname $0)"; pwd)
+resultdir=$testdir/result
+if [ ! -d $resultdir ]; then
+	mkdir -p $resultdir
+fi
+
+# run test graph
+pytest $testdir/cupy/tests/cupy_tests/cuda_tests/test_graph.py >& $result/result_test_graph
+
+# run test raw
+pytest $testdir/cupy/tests/cupyx_tests/jit_tests/test_raw.py -k "not test_device_cache" >& $result/result_test_raw
+
+# run test device function
+pytest $testdir/cupy/tests/cupyx_tests/jit_tests/test_device_function.py >& $result/result_test_device_function
+
+# run test pinned array
+pytest $testdir/cupy/tests/cupyx_tests/test_pinned_array.py >& $result/result_test_pinned_array
