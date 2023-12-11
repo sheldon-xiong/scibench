fd3fb444c6c3e40b220bdacf590865ed8204628e
johnzlli <1016963088@qq.com>
2023-12-11 10:18:44 +0800

    fix bash

--- cupy_test/run.bash
+++ cupy_test/run.bash
@@ -6,13 +6,13 @@ if [ ! -d $resultdir ]; then
 fi
 
 # run test graph
-pytest $testdir/cupy/tests/cupy_tests/cuda_tests/test_graph.py >& $result/result_test_graph
+pytest $testdir/cupy/tests/cupy_tests/cuda_tests/test_graph.py >& $resultdir/result_test_graph
 
 # run test raw
-pytest $testdir/cupy/tests/cupyx_tests/jit_tests/test_raw.py -k "not test_device_cache" >& $result/result_test_raw
+pytest $testdir/cupy/tests/cupyx_tests/jit_tests/test_raw.py -k "not test_device_cache" >& $resultdir/result_test_raw
 
 # run test device function
-pytest $testdir/cupy/tests/cupyx_tests/jit_tests/test_device_function.py >& $result/result_test_device_function
+pytest $testdir/cupy/tests/cupyx_tests/jit_tests/test_device_function.py >& $resultdir/result_test_device_function
 
 # run test pinned array
-pytest $testdir/cupy/tests/cupyx_tests/test_pinned_array.py >& $result/result_test_pinned_array
+pytest $testdir/cupy/tests/cupyx_tests/test_pinned_array.py >& $resultdir/result_test_pinned_array
